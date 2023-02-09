c0117bdcc2b77133c2554e54b06b0717667301bb
Kyle <kyled073@gmail.com>
2023-02-09 17:16:10 +0000

    Inversion Support for Camera

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public float mouseSens = 250.0f;     // to be set externally by player
+    private float mouseSens = 250.0f;     // to be set externally by player
     public Transform playerBody;
     float xRotate = 0.0f;               // the rotation to be applied to the player
+    int inverted = -1;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        changeInversion(false);
     }
 
     // Update is called once per frame
@@ -22,11 +24,21 @@ public class PlayerCamera : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
 
         // clamp the rotation to prevent the player from looked too far up or down
-        xRotate -= mouseY;
+        xRotate += mouseY * inverted;
         xRotate = Mathf.Clamp(xRotate, -135.0f, 135.0f);
 
         // rotate camera and body
         transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    // helper function allows for sensitivity to be set
+    void changeSens(float newSens) {
+        mouseSens = newSens;
+    }
+
+    // this function allows for inverted camera control if player wishes
+    void changeInversion(bool isInvert) {
+        inverted = isInvert ? 1 : -1;
     }
 }
