60347c796f0b3981d0a5d37fed1227e953872017
Kyle <kyled073@gmail.com>
2023-02-09 17:08:41 +0000

    First Person Camera

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float mouseSens = 250.0f;     // to be set externally by player
+    public Transform playerBody;
+    float xRotate = 0.0f;               // the rotation to be applied to the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the location of the mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        // clamp the rotation to prevent the player from looked too far up or down
+        xRotate -= mouseY;
+        xRotate = Mathf.Clamp(xRotate, -135.0f, 135.0f);
+
+        // rotate camera and body
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
