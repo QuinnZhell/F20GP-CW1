672ffff1def2bcd435e0f4f9c167d75f73d7bf89
kd41 <kyled073@gmail.com>
2023-02-18 23:01:41 +0000

    Basic Player Movement
    Added basic movement in a 3D Space.
    A model has also been added to act as the playable character, source: https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/free-low-poly-human-rpg-character-219979

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    private float mouseSens = 250.0f;     // to be set externally by player
+    public float mouseSens = 300.0f;     // to be set externally by player
     public Transform playerBody;
     float xRotate = 0.0f;               // the rotation to be applied to the player
     int inverted = -1;
@@ -19,6 +19,16 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // process player mouse movements
+        Look();
+    }
+
+    // helper function allows for sensitivity to be set
+    void changeSens(float newSens) {
+        mouseSens = newSens;
+    }
+
+    void Look() {
         // get the location of the mouse
         float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
@@ -30,11 +40,6 @@ public class PlayerCamera : MonoBehaviour
         // rotate camera and body
         transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-    }
-
-    // helper function allows for sensitivity to be set
-    void changeSens(float newSens) {
-        mouseSens = newSens;
     }
 
     // this function allows for inverted camera control if player wishes
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header ("External Assignments")]
+    Transform body;
+    public new Camera camera;
+    CharacterController controller;
+
+    [Header ("Movement Stats")]
+    public float moveSpeed = 2.5f;
+    public float sprintSpeed = 4f;
+    private bool isSprinting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.transform;
+        controller = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // is the player trying to sprint?
+        if(Input.GetButton("Sprint"))
+            isSprinting = true;
+        else
+            isSprinting = false;
+
+        // move the player
+        movePlayer();
+    }
+
+    void movePlayer(){
+
+        // get the axis input and assign appropriate names
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        float moveZ = Input.GetAxis("Forward");
+
+        // takes the position the player is facing, creates a vector pointing in desired direction
+        Vector3 move = transform.right * moveX + transform.up * moveY + camera.transform.forward * moveZ;
+
+        controller.Move(move * (isSprinting ? sprintSpeed : moveSpeed) * Time.deltaTime);
+    }
+}
