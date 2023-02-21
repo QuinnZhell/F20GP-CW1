4a6a5210bd647a8677c12f5ef40c8755c782337b
JamesBeach2 <james.beach.98@gmail.com>
2023-02-21 00:23:46 +0000

    add somewhat final crab procedural animation

---
+++ Assets/Scripts/Crab/IKFootSolver.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS SCRIPT IS ADAPTED AND UPDATED FROM THIS TUTORIAL FROM UNITY TEAM
+// https://www.youtube.com/watch?v=acMK93A-FSY
+
+public class IKFootSolver : MonoBehaviour
+{
+    [SerializeField] LayerMask terrainLayer = default;
+    [SerializeField] Transform body = default;
+    [SerializeField] Transform leg = default;
+    [SerializeField] IKFootSolver otherFoot = default;
+    private float speed = 4;
+    private float stepDistance = 1;
+    private float stepLength = 1;
+    private float stepHeight = 0.5f;
+    private Vector3 footOffset = default;
+    private Vector3 legDir = default;
+    float footSpacing;
+    Vector3 oldPosition, currentPosition, newPosition;
+    Vector3 oldNormal, currentNormal, newNormal;
+    float lerp;
+
+    private void Start()
+    {
+        footSpacing = transform.localPosition.x;
+        currentPosition = newPosition = oldPosition = transform.position;
+        currentNormal = newNormal = oldNormal = transform.up;
+        lerp = 1;
+        footOffset = body.position - leg.position - Vector3.one;
+        legDir = (leg.position - body.position).normalized;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        transform.position = currentPosition;
+        transform.up = currentNormal;
+
+        Ray ray = new Ray(body.position - footOffset, legDir);
+
+        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
+        {
+            if (Vector3.Distance(newPosition, info.point) > stepDistance && !otherFoot.IsMoving() && lerp >= 1)
+            {
+                lerp = 0;
+                int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPosition).z ? 1 : -1;
+                newPosition = info.point + (body.forward * stepLength * direction) + footOffset;
+                newNormal = info.normal;
+            }
+        }
+
+        if (lerp < 1)
+        {
+            Vector3 tempPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            tempPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = tempPosition;
+            currentNormal = Vector3.Lerp(oldNormal, newNormal, lerp);
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+            oldNormal = newNormal;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawSphere(newPosition, 0.5f);
+    }
+
+
+
+    public bool IsMoving()
+    {
+        return lerp < 1;
+    }
+
+
+
+}
