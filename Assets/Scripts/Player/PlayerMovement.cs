32a9611ca80ecd6ebd6ebb8b4459086c494f49ad
kd41 <kyled073@gmail.com>
2023-03-01 18:45:12 +0000

    Player can walk on solid land

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+[System.Serializable] public class PlayerMovement : MonoBehaviour
 {
     [Header ("External Assignments")]
     Transform body;
@@ -17,16 +17,37 @@ public class PlayerMovement : MonoBehaviour
     private bool isSwimming;
     [SerializeField] LayerMask waterMask;
 
+    [Header ("Grounded Behaviour")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] Vector3 velocity;
+    [SerializeField] float gravity = -6f;
+    [SerializeField] float jumpHeight = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
         body = this.transform;
+        isSwimming = false;
         controller = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(!isSwimming) {
+            PerformGroundCheck();
+
+            if(Input.GetButtonDown("Jump") && isGrounded)
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+
         // is the player trying to sprint?
         if(Input.GetButton("Sprint"))
             isSprinting = true;
@@ -35,6 +56,14 @@ public class PlayerMovement : MonoBehaviour
 
         // move the player
         movePlayer();
+    }
+
+    void PerformGroundCheck() {
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
+        Debug.Log(isGrounded);
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
     }
 
     void movePlayer(){
