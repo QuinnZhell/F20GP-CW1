3c9b16d63a18d8b7f35503676e9a1ea7a0175f00
kd41 <kyled073@gmail.com>
2023-03-01 19:33:14 +0000

    Player can now walk or swim.
    Depending on area the fog density and color will change

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,8 +10,8 @@ using UnityEngine;
     CharacterController controller;
 
     [Header ("Movement Stats")]
-    public float moveSpeed = 2.5f;
-    public float sprintSpeed = 4f;
+    public float moveSpeed;
+    public float sprintSpeed;
     private bool isSprinting = false;
 
     private bool isSwimming;
@@ -30,22 +30,36 @@ using UnityEngine;
     void Start()
     {
         body = this.transform;
-        isSwimming = false;
+        isSwimming = true;
         controller = this.GetComponent<CharacterController>();
+        moveSpeed = 5f;
+        sprintSpeed = 8f;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        // player has two states, swimming and walking
+        // these are the walking behaviours
         if(!isSwimming) {
+            // check the player is touching the ground
             PerformGroundCheck();
 
+            // if grounded they are able to jump
             if(Input.GetButtonDown("Jump") && isGrounded)
                 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 
+            // apply gravity
             velocity.y += gravity * Time.deltaTime;
             controller.Move(velocity * Time.deltaTime);
+        }
+
+        // player should slightly sink underwater
+        if(isSwimming) {
+            velocity.y = -0.5f;
+            controller.Move(velocity * Time.deltaTime);
+            
         }
 
         // is the player trying to sprint?
@@ -55,18 +69,21 @@ using UnityEngine;
             isSprinting = false;
 
         // move the player
-        movePlayer();
+        MovePlayer();
     }
 
+    // checks that the player is in contact with the floor
     void PerformGroundCheck() {
         isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
         Debug.Log(isGrounded);
+
+        // make sure the players velocity doesn't not continually decrease due to gravity
         if (isGrounded && velocity.y < 0) {
             velocity.y = -2f;
         }
     }
 
-    void movePlayer(){
+    void MovePlayer(){
 
         // get the axis input and assign appropriate names
         float moveX = Input.GetAxis("Horizontal");
@@ -77,5 +94,21 @@ using UnityEngine;
         Vector3 move = transform.right * moveX + transform.up * moveY + camera.transform.forward * moveZ;
 
         controller.Move(move * (isSprinting ? sprintSpeed : moveSpeed) * Time.deltaTime);
+    }
+
+    public void StartSwimming() {
+        Debug.Log("I am swimming");
+        
+        isSwimming = true;
+        RenderSettings.fogColor = new Color32(49,127,171,255);
+        RenderSettings.fogDensity = 0.015f;
+    }
+
+    public void StartWalking() {
+        Debug.Log("I am walking");
+
+        isSwimming = false;
+        RenderSettings.fogColor = new Color32(0,0,0,255);
+        RenderSettings.fogDensity = 0.1f;
     }
 }
